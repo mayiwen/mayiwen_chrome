fa18b06b8873af9f72041c03fd86ffd9cc5ebd1a
mayiwen <i@mayiwen.com>
2023-01-01 23:25:27 +0800

    #20230101#  添加alt

--- js/content.js
+++ js/content.js
@@ -1,7 +1,9 @@
 console.log('这是content打印的内容')
 document.onkeydown=function(e){    //对整个页面监听  
-    var keyNum=window.event ? e.keyCode :e.which;       //获取被按下的键值  
-    if(chrome && chrome.runtime && e.target && e.target.nodeName !== 'INPUT'){
+    var keyNum=window.event ? e.keyCode :e.which;       //获取被按下的键值 
+    console.log('这是e') 
+    console.log(e)
+    if(chrome && chrome.runtime && e.target && e.target.nodeName !== 'INPUT' && e.altKey){
         chrome.runtime.sendMessage({type: 'key', key: e.key}, function(response){
             console.log('这是content 返回的')
             console.log(response)
